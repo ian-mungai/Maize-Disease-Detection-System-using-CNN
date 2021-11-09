1ca35dd4731d4ad7a1defc3c3ee78e9916a74274
ian-mungai <ian.mungai@icloud.com>
2021-11-09 12:34:02 +0300

    Logout Error Fixed

--- web-app/app/Http/Controllers/Auth/LoginController.php
+++ web-app/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use \Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -32,11 +33,11 @@ class LoginController extends Controller
     {
             return '/dashboard';
     }
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+    protected function loggedOut(Request $request)
+    {
+        return redirect('/login');
+    }
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
